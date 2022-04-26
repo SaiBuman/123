f11a11fdf28aace50d0339842cf7e31dc569f569
Sai Buman <78092854+SaiBuman@users.noreply.github.com>
2022-04-26 19:32:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
  function setup() {
-     canvas = createCanvas(500,500);
-     canvas.position(800,125)
+     canvas = createCanvas(400,400);
+     canvas.position(800,125);
+     video = createCapture(VIDEO);
+     video.size(400,400);
+     video.position(200,125);
+     poseNet=ml5.poseNet(video,modelLoaded);
+     poseNet.on("pose",gotPoses)
+     }
+
+     function draw() {
+        background("#4a875b");  
+     }
+
+     function modelLoaded() {
+         console.log("Model is Loaded");
+     }
+
+     function gotPoses(results) {
+         if (results.length>0) {
+             console.log(results);
+         }
      }
