4eaa565f803bf79a84173365b6559a00fb7a0827
Sai Buman <78092854+SaiBuman@users.noreply.github.com>
2022-04-28 19:38:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+nose_x = "" ;
+nose_y = "" ;
+
+left_wrist_x = "" ;
+right_wrist_x = "" ;
+
+difference = "" ;
+
  function setup() {
      canvas = createCanvas(400,400);
      canvas.position(800,125);
@@ -10,6 +18,9 @@
 
      function draw() {
         background("#4a875b");  
+        fill("#fa483e");
+        stroke("fa483e");
+        square(nose_x,nose_y,difference)
      }
 
      function modelLoaded() {
@@ -19,5 +30,11 @@
      function gotPoses(results) {
          if (results.length>0) {
              console.log(results);
+             nose_x = results[0].pose.nose.x;
+             nose_y = results[0].pose.nose.y;
+
+             left_wrist_x = results[0].pose.leftWrist.x;
+             right_wrist_x = results[0].pose.rightWrist.y;
+             difference = floor(left_wrist_x - right_wrist_x);
          }
      }
